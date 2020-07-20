59e6d4d8aebed9dae8ae98edd67eba3b149f5029
Yaktas <joswhitestone@gmail.com>
2020-07-20 11:30:58 -0500

    second try

--- src/test/java/GitProject.java
+++ src/test/java/GitProject.java
@@ -1,6 +1,7 @@
 public class GitProject {
-}
+
 
     public static void main(String[] args) {
         System.out.println("Git Practice");
     }
+}
---
+++ src/test/java/secondGitfiles.java
@@ -0,0 +1,5 @@
+public class secondGitfiles {
+    public static void main(String[] args) {
+        System.out.println("second try");
+    }
+}
